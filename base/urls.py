f3311b5c04637a7360ef4d8e0304d0ef19508444
Nikita Turda <nikitosturda5@gmail.com>
2023-04-03 23:15:51 +0300

    Authentication is fully completed (registration, login)

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . import views
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, Login, Registration
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', views.TaskList.as_view(), name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('login/', Login.as_view(), name='login'),
+    path('registration/', Registration.as_view(), name='registration'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('tasks/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task_create/', TaskCreate.as_view(), name='task_create'),
     path('task_update/<int:pk>/', TaskUpdate.as_view(), name='task_update'),
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -82,3 +82,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
